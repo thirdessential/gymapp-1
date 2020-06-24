ca38cf8d136340f1dfde565e67a155741797b0b4
yatan.vesh <yatan.vesh@gmail.com>
2020-06-24 16:49:53 +0530

    Comment our rnCallkeep, install rn push notiff

--- src/utils/callKeep.js
+++ src/utils/callKeep.js
@@ -1,15 +1,15 @@
-
-export const randomuuid = 'randomuuid'
-
-export const callKeepConfig = {
-  ios: {
-    appName: 'My app name',
-  },
-  android: {
-    alertTitle: 'Permissions required',
-    alertDescription: 'This application needs to access your phone accounts',
-    cancelButton: 'Cancel',
-    okButton: 'ok',
-  }
-};
-
+//
+// export const randomuuid = 'randomuuid'
+//
+// export const callKeepConfig = {
+//   ios: {
+//     appName: 'My app name',
+//   },
+//   android: {
+//     alertTitle: 'Permissions required',
+//     alertDescription: 'This application needs to access your phone accounts',
+//     cancelButton: 'Cancel',
+//     okButton: 'ok',
+//   }
+// };
+//
