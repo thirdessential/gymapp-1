27035488f0d2ee614e842cf1f70486ca20696540
Devesh Gupta <70230227+Dgdevesh@users.noreply.github.com>
2020-09-14 18:14:40 +0530

    some changes in setting.gradel

--- android/app/src/main/java/com/thirdessential/fitnessfirst/MainApplication.java
+++ android/app/src/main/java/com/thirdessential/fitnessfirst/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.agora.rtc.react.RCTAgoraRtcPackage;
 
 import net.no_mad.tts.TextToSpeechPackage;
 import com.facebook.react.ReactInstanceManager;
